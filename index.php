fb7e99ab382956af94109aff4415f8f02f3680ca
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-01-24 17:52:46 +0100

    password in una pagina

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $lunghezza= $_GET["length"];
 function randomPassword($pwslen) {
     $caratteri="1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuioplkjhgfdsaxzcvbnm!/£$%&/=?*+-_";
     $password="";
-    for ($i=0 ; $i <= $pwslen; $i++){
+    for ($i=0 ; $i < $pwslen; $i++){
      
         $ind_random = rand(0, strlen($caratteri)-1);
         $car_random = $caratteri[$ind_random];
